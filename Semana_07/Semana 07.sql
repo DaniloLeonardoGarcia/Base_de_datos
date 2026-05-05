a39bc638dd99b4678c5d8905ad6b000429ee2232
DaniloLeonardoGarcia <daniloleonardogarciavizcardo@gmail.com>
2026-05-05 07:41:02 -0500

    Add files via upload

---
+++ Semana_07/Semana 07.sql
@@ -0,0 +1,25 @@
+create database Deudas
+
+use Deudas
+
+create table deuda(
+deudor varchar(50),
+mes varchar(50),
+monto int 
+)
+
+
+insert into deuda(deudor,mes,monto) values
+('William', 'Enero', 500),
+('Diego', 'Abril', 300),
+('David', 'Febrero', 450);
+
+Select *
+from(
+	select deudor, mes, monto
+	from deuda
+) as fuente 
+pivot(
+sum(monto)
+for mes in([Enero],[Febrero],[Abril])
+)as pivotTable;
