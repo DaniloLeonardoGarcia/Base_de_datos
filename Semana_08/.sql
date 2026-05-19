360becaf3777331b79485910bb08ac6bf19820f3
DaniloLeonardoGarcia <daniloleonardogarciavizcardo@gmail.com>
2026-05-19 07:41:45 -0500

    Migrate SQL functions and procedures to new syntax

--- Semana_08/.sql
+++ Semana_08/.sql
@@ -1,49 +1,50 @@
-CREATE FUNCTION CalcularPuntualidad (hora_ingreso DATETIME) 
+CREATE FUNCTION CalcularPuntualidad (@hora_ingreso DATETIME)
 RETURNS VARCHAR(20)
-DETERMINISTIC
+AS
 BEGIN
-    DECLARE estado_puntualidad VARCHAR(20);
-    
-    IF TIME(hora_ingreso) > '08:00:00' THEN
-        SET estado_puntualidad = 'Tardanza';
+    DECLARE @estado_puntualidad VARCHAR(20);
+
+    IF CONVERT(TIME, @hora_ingreso) > '08:00:00'
+        SET @estado_puntualidad = 'Tardanza';
     ELSE
-        SET estado_puntualidad = 'Puntual';
-    END IF;
-    
-    RETURN estado_puntualidad;
-END //
+        SET @estado_puntualidad = 'Puntual';
 
-DELIMITER ;
+    RETURN @estado_puntualidad;
+END;
 
-CREATE PROCEDURE RegistrarEntradaPersonal (
-    IN p_InstructorID INT,
-    IN p_CodigoIngreso VARCHAR(20)
-)
+CREATE PROCEDURE RegistrarEntradaPersonal
+    @p_InstructorID INT,
+    @p_CodigoIngreso VARCHAR(20)
+AS
 BEGIN
-    DECLARE v_EstadoCalculado VARCHAR(20);
-    DECLARE v_HoraActual DATETIME;
+    SET NOCOUNT ON;
 
-    DECLARE EXIT HANDLER FOR SQLEXCEPTION
-    BEGIN
-        ROLLBACK;
+    DECLARE @v_EstadoCalculado VARCHAR(20);
+    DECLARE @v_HoraActual DATETIME = GETDATE();
+
+    BEGIN TRY
+        BEGIN TRANSACTION;
+
+        SET @v_EstadoCalculado = dbo.CalcularPuntualidad(@v_HoraActual);
+
+        INSERT INTO RegistroAsistencia (InstructorID, CodigoValidacion, HoraEntrada, EstadoAsistencia)
+        VALUES (@p_InstructorID, @p_CodigoIngreso, @v_HoraActual, @v_EstadoCalculado);
+
+        UPDATE Instructores
+        SET EstadoOperativo = 'En Instalaciones'
+        WHERE ID = @p_InstructorID;
+
+        COMMIT TRANSACTION;
+
+        SELECT CONCAT('Asistencia registrada correctamente. Estado: ', @v_EstadoCalculado) AS MensajeOperacion;
+
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+
         SELECT 'Error al registrar la asistencia. Se han revertido los cambios.' AS MensajeOperacion;
-    END;
-
-    SET v_HoraActual = NOW();
-    
-    SET v_EstadoCalculado = CalcularPuntualidad(v_HoraActual);
-
-    START TRANSACTION;
-
-    INSERT INTO RegistroAsistencia (InstructorID, CodigoValidacion, HoraEntrada, EstadoAsistencia)
-    VALUES (p_InstructorID, p_CodigoIngreso, v_HoraActual, v_EstadoCalculado);
-
-    UPDATE Instructores
-    SET EstadoOperativo = 'En Instalaciones'
-    WHERE ID = p_InstructorID;
-
-    COMMIT;
-    
-    SELECT CONCAT('Asistencia registrada correctamente. Estado: ', v_EstadoCalculado) AS MensajeOperacion;
-
+        -- Opcional: puedes lanzar el error original si quieres más detalle
+        -- THROW;
+    END CATCH
 END;
